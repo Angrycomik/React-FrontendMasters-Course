1fe5fe279280473b181d85d87ab860e1b050d8c9
Angrycomik <angrycomik@gmail.com>
2026-01-12 21:25:36 +0100

    v1

---
+++ src/App.js
@@ -0,0 +1,33 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "Fish pizza",
+      description: "Tuna flavoured",
+    }),
+    React.createElement(Pizza, {
+      name: "Hawaiian pizza",
+      description: "Pineapple flavoured",
+    }),
+    React.createElement(Pizza, {
+      name: "Pepperoni pizza",
+      description: "speaks for itself",
+    }),
+    React.createElement(Pizza, {
+      name: "American Pizza",
+      description: "French fries and hot dogs",
+    })
+  );
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
