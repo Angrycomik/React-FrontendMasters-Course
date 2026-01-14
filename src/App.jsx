075bf029867271ece0719c28075d3a3a2b84ed18
Angrycomik <angrycomik@gmail.com>
2026-01-14 20:24:48 +0100

    v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1> Padre Gino's Pizzeria</h1>
-      <Pizza
-        name="pepperoni"
-        description="tasty pizza"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="american"
-        description="with fries"
-        image={"/public/pizzas/big_meat.webp"}
-      />
-      <Pizza
-        name="hawaiian"
-        description="with pineapples"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
+      <Order />
     </div>
   );
 };
